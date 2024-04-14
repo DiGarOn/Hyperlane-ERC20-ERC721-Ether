6cf09d03fe37dd04d1f41f8a293d353271d52c58
DmitriyG <garkin.dmitriy2004@gmail.com>
2024-04-14 23:44:17 +0300

    signatures

--- contracts/ETH_H.sol
+++ contracts/ETH_H.sol
@@ -6,12 +6,16 @@ import { IMailbox } from "../interfaces/IMailbox.sol";
 import "hardhat/console.sol";
 
 contract ETH_H {
-    IMailbox mailBox;
+    IMailbox public mailBox;
+    uint32 public chainId;
+    address public owner;
 
     event Transfered(uint256 amount);
 
-    constructor (address mailBox_) payable {
+    constructor (address mailBox_, uint32 chainId_) payable {
         mailBox = IMailbox(mailBox_);
+        chainId = chainId_;
+        owner = msg.sender;
     }
 
     receive() external payable {}
@@ -36,9 +40,56 @@ contract ETH_H {
         return fee;
     }
 
-    function remoteTransfer(uint32 destination, address recipient_, uint256 amount) external payable {
+    function withPrefix(bytes32 _hash) private pure returns(bytes32) {
+        return keccak256(
+            abi.encodePacked(
+                "\x19Ethereum Signed Message:\n32",
+                _hash
+            )
+        );
+    }
+
+    function recoverSigner(bytes32 message, bytes memory signature) private pure returns(address) {
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(signature);
+
+        return ecrecover(message, v, r, s);
+    }
+
+    function splitSignature(bytes memory signature) private pure returns(uint8 v, bytes32 r, bytes32 s) {
+        require(signature.length == 65);
+
+        assembly {
+            r := mload(add(signature, 32))
+
+            s := mload(add(signature, 64))
+
+            v := byte(0, mload(add(signature, 96)))
+        }
+
+        return(v, r, s);
+    }
+
+    function remoteTransfer(uint32 destination, address recipient_, uint256 amount, uint256 maticToEth, bytes memory signature) external payable {
+        uint256 amount_to_receive;
+        if( destination == 137 ||  chainId == 137) {
+            bytes32 message = withPrefix(keccak256(abi.encodePacked(
+                maticToEth
+            )));
+            require(
+                recoverSigner(message, signature) == owner, "invalid sig!"
+            );
+
+            if( destination != 137 ) { // send from poligon
+                amount_to_receive = amount * maticToEth;
+            } else { // send to poligon
+                amount_to_receive = amount * 10**18 / maticToEth;
+            }
+        } else {
+            amount_to_receive = amount;
+        }
+
         bytes32 _recipient = addressToBytes32(recipient_);
-        bytes memory body = uint_to_bytes(amount, msg.sender);
+        bytes memory body = uint_to_bytes(amount_to_receive, msg.sender);
         uint256 fee = mailBox.quoteDispatch(destination, _recipient, body);
         console.log("fee is: ", fee);
         require(msg.value >= fee + amount);
--- test/ETH_H.js
+++ test/ETH_H.js
@@ -14,7 +14,17 @@ async function main() {
   const contract = Token.attach("0xca191222Ae8A6cc19D245E4829501D39aa9Cc228"); // bnbtest
   console.log("Contract address:", contract.target);
 //   await contract.connect(deployer).remoteTransfer(97,"0xca191222Ae8A6cc19D245E4829501D39aa9Cc228", 1000, {value: ethers.parseEther("0.005")}); // to bnb
-  await contract.connect(deployer).remoteTransfer(11155111,"0xBD6cCCBb8E4D6457e58953D1b3f1aA4D8f2522cC", 1000, {value: ethers.parseEther("0.005")}); // sepolia
+  await contract.connect(deployer).remoteTransfer(11155111,"0xBD6cCCBb8E4D6457e58953D1b3f1aA4D8f2522cC", 1000, 0, 0, "0", {value: ethers.parseEther("0.005")}); // sepolia
+
+  const maticToEth = 222522764413594n;
+  const hash = ethers.utils.solidityKeccak256(
+    ["uint256"],
+    [maticToEth]
+  );
+
+  const messageHashBin = ethers.utils.arrayify(hash);
+  const signature = await deployer.signMessage(messageHashBin);
+  await contract.connect(deployer).remoteTransfer(137,"0xBD6cCCBb8E4D6457e58953D1b3f1aA4D8f2522cC", 1000, maticToEth, signature, {value: ethers.parseEther("0.005")}); // sepolia
 
   console.log(await provider.getBalance("0x095454F216EC9485da86D49aDffAcFD0Fa3e5BE5"));
 }
