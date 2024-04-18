fc1f1c090d6a21c80ae48a6d98ecb6469afc095f
DmitriyG <garkin.dmitriy2004@gmail.com>
2024-04-18 09:57:03 +0300

    correct signature

--- test/ETH_H.js
+++ test/ETH_H.js
@@ -32,18 +32,18 @@ async function main() {
 //   await contract.connect(deployer).remoteTransfer(scroll_chainId,"0xBD6cCCBb8E4D6457e58953D1b3f1aA4D8f2522cC", 1000, 0, "0", {value: ethers.parseEther("0.005")}); // sepolia
 
 // // пример создания подписи (она может быть выложена на сайт вместе с соотношением matic/eth)
-//   const maticToEth = 222522764413594n; // соотношение
-//   const hash = ethers.utils.solidityKeccak256(
-//     ["uint256"],
-//     [maticToEth]
-//   );
+  const maticToEth = 222522764413594n; // соотношение
+  const hash = ethers.solidityPackedKeccak256(
+    ["uint256"],
+    [maticToEth]
+  );
 
-//   const messageHashBin = ethers.utils.arrayify(hash);
-//   const signature = await deployer.signMessage(messageHashBin); // важно, что подпись происходит ончейн!
-
+  const messageHashBin = ethers.getBytes(hash);
+  const signature = await deployer.signMessage(messageHashBin); // важно, что подпись происходит ончейн!
+  console.log(signature)
 // // пример вызова функции при работе с полигоном (отправка с или на полигон)
 // // параметры: id сети, адрес контракта в сети, объем, соотношение, подпись
-//   await contract.connect(deployer).remoteTransfer(polygon_chainId,"0xBD6cCCBb8E4D6457e58953D1b3f1aA4D8f2522cC", 1000, maticToEth, signature, {value: ethers.parseEther("0.005")}); // sepolia
+  await contract.connect(deployer).remoteTransfer(polygon_chainId,"0xBD6cCCBb8E4D6457e58953D1b3f1aA4D8f2522cC", 1000, maticToEth, signature, {value: ethers.parseEther("0.005")}); // sepolia
 
 //   console.log(await provider.getBalance("0x095454F216EC9485da86D49aDffAcFD0Fa3e5BE5"));
 }
